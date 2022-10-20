c52069348a0b4c62522614050ac471aebbb56b61
RafaelFronteira <rafael_fronteira@hotmail.com>
2022-10-20 15:53:22 -0300

    save

--- assets/js/mainConfig.js
+++ assets/js/mainConfig.js
@@ -1,5 +1,5 @@
-// const socket = io('https://projeto-metaverso.herokuapp.com/');
-const socket = io('http://localhost:3000');
+const socket = io('https://projeto-metaverso.herokuapp.com/');
+// const socket = io('http://localhost:3000');
 let playerID = "";
 
 // executar para limpar a lista de players n jogaveis do server
